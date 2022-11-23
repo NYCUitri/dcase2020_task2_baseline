2cb3f4ae1d52b2006fbb9fc0028720e2da9db0fd
ting0602 <ting0602.cs09@nctu.edu.tw>
2022-11-23 13:39:33 +0800

    feat: rec_func

--- reconstruct_img.py
+++ reconstruct_img.py
@@ -17,11 +17,16 @@ def reconstruct_spectrogram(reconstructions, name=None):
         else:
             img.save("./rec_imgs/"+str(i)+".png")
 
-def latent_distrbution(latents_list, name=""):
-    for latent in latents_list:
+def latent_distrbution(latents_list, labels=None, name=""):
+    if labels is None:
+        labels = []
+        for i in range(len(latents_list)):
+            labels.append(i)
+    for i, latent in enumerate(latents_list):
         latent = latent.detach().cpu().numpy()
         latent = latent.reshape((-1, 2))
         print(latent.shape)
         print(latent[:,0].shape, latent[:,1].shape)
-        plt.scatter(latent[:,0],latent[:,1], s=3, alpha=0.5)
+        plt.scatter(latent[:,0],latent[:,1], s=3, alpha=0.5, labels=labels[i])
+    plt.legend()
     plt.savefig("./rec_imgs/distrbution{}.png".format(name))
